40c45f5dd4ebc84bdde6c1fe632cfa9b635fff87
patrickgsda <patrickarruda30@gmail.com>
2022-01-02 20:37:42 -0300

    feat: flask routers / and current_datetime

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
-# Código do dev aqui
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    my_dict = {'data': 'Hello Flask'}
+    return my_dict
+
+@app.route('/current_datetime', methods=['GET'])
+def current_datetime():
+    date = datetime.now()
+    hour = int(date.strftime('%H'))
+    if hour < 12:
+        message = 'Bom dia!'
+    elif hour >= 12 and hour <= 18:
+        message = 'Boa tarde!'
+    else:
+        message = 'Boa noite!'
+        
+    dict_return = {'current_datetime': date, 'message': message}
+    return dict_return
